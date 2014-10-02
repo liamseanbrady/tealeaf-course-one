c41494526f7cd1f67a34e6db037972db3efe05e0
Liam Sean Brady <liamseanbrady@gmail.com>
2014-10-02 21:25:12 +0100

    Add functionality for playing game, with player 2 choosing random empty square to put O in

--- Lesson1/TicTacToeAssignment/tictactoe_attempt.rb
+++ Lesson1/TicTacToeAssignment/tictactoe_attempt.rb
@@ -31,30 +31,34 @@ def input_valid?(board_model, p1_move)
   board_model[p1_move.to_i - 1] != "X" && board_model[p1_move.to_i - 1] != "O" && p1_move.to_i < 10
 end
 
-board_model = [ "1", "2", "3", 
-                "4", "5", "6",
-                "7", "8", "9" ]
+board_model = [ " ", " ", " ", 
+                " ", " ", " ",
+                " ", " ", " " ]
 
 p board_model
 
 # Display the empty board
 
 # While there are still empty spaces and there is no winner
-  # # Ask player 1 to pick a square between 1 and 9
-  # puts "Choose a position (from 1 to 9) to place a piece"
-  # # Store player 1's input
-  # p1_move = gets.chomp
-  # # If square is empty, then fill it
-  # if board_model[p1_move.to_i - 1] != "X" && board_model[p1_move.to_i - 1] != "O"
-  #   board_model[p1_move.to_i - 1] = "X"
-  # # Else tell player 1 that square is taken, and ask them again to pick another square between 1 and 9
-  # else
-  #   puts "Whoops! That spot's taken. Please try again..."
-  # end
+while board_model.select { |square| square == " " }
   player_1_move(board_model)
   # if there are greater than 2 empty squares
-    # Pick a random square for the empty squares and put an O in it for player 2
-  # End the game and declare who won
+    # Check if there's a winner
+  # Pick a random square for the empty squares and put an O in it for player 2
+  # Filter out the squares that are not empty
+  empty_squares = []
+  board_model.each_with_index do |symbol, position|
+    if symbol == " "
+      empty_squares.push([symbol, position])
+    end
+  end
+  # Pick a random square from those available
+  rand_square = empty_squares[rand(empty_squares.size - 1)]
+  # Put a O in the chosen square
+  board_model[rand_square[1]] = "O"
+  p board_model
+end
+# End the game and declare who won
 
 
 # Separate logic for finding a winner
@@ -67,7 +71,4 @@ p board_model
   # current, current + 2, current + 4 are all the same value
 # For indices 1, 4, and 7 in the array, we have a winner if the following is true:
   # current, current + 1, current + 2 are all the same value
-
-
-p board_model
 
