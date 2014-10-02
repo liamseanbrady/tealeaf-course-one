ecf5cb39b17a83281dc212c9c652edb15a66ccfc
Liam Sean Brady <liamseanbrady@gmail.com>
2014-10-02 19:34:26 +0100

    Include functionality for player 1 putting one X in board_model

--- Lesson1/TicTacToeAssignment/tictactoe_attempt.rb
+++ Lesson1/TicTacToeAssignment/tictactoe_attempt.rb
@@ -12,18 +12,48 @@
 #   x  |  x  |  x  
 #      |     |     
 
+def player_1_move(board_model)
+  # Ask player 1 to pick a square between 1 and 9
+  puts "Choose a position (from 1 to 9) to place a piece"
+  # Store player 1's input
+  p1_move = gets.chomp
+  # If square is empty, then fill it
+  if input_valid?(board_model, p1_move)
+    board_model[p1_move.to_i - 1] = "X"
+  # Else tell player 1 that square is taken, and ask them again to pick another square between 1 and 9
+  else
+    puts "Whoops! That spot's taken. Please try again..."
+    player_1_move(board_model)
+  end
+end
 
+def input_valid?(board_model, p1_move)
+  board_model[p1_move.to_i - 1] != "X" && board_model[p1_move.to_i - 1] != "O" && p1_move.to_i < 10
+end
 
+board_model = [ "1", "2", "3", 
+                "4", "5", "6",
+                "7", "8", "9" ]
+
+p board_model
 
 # Display the empty board
 
 # While there are still empty spaces and there is no winner
-  # Ask player 1 to pick a square between 1 and 9
-  # Store player 1's input
-    # If square is empty, then fill it
-    # Elsif tell player 1 that square is taken, and ask them again to pick another square between 1 and 9
-    # if there are greater than 2 empty squares
-      # Pick a random square for the empty squares and put an O in it for player 2
+  # # Ask player 1 to pick a square between 1 and 9
+  # puts "Choose a position (from 1 to 9) to place a piece"
+  # # Store player 1's input
+  # p1_move = gets.chomp
+  # # If square is empty, then fill it
+  # if board_model[p1_move.to_i - 1] != "X" && board_model[p1_move.to_i - 1] != "O"
+  #   board_model[p1_move.to_i - 1] = "X"
+  # # Else tell player 1 that square is taken, and ask them again to pick another square between 1 and 9
+  # else
+  #   puts "Whoops! That spot's taken. Please try again..."
+  # end
+  player_1_move(board_model)
+  # if there are greater than 2 empty squares
+    # Pick a random square for the empty squares and put an O in it for player 2
   # End the game and declare who won
 
 
@@ -39,5 +69,5 @@
   # current, current + 1, current + 2 are all the same value
 
 
-
+p board_model
 
