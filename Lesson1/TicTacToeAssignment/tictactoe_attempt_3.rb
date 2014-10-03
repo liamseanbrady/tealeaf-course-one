1819c648c619f7f519e1759dfa06e5daf3d695f4
Liam Sean Brady <liamseanbrady@gmail.com>
2014-10-03 21:54:38 +0100

    Add tictactoe_attempt_3, based on the pseudo-code from the solution video

---
+++ Lesson1/TicTacToeAssignment/tictactoe_attempt_3.rb
@@ -0,0 +1,12 @@
+# draw a board
+
+# loop until a winner or all squares are taken
+#   player 1 picks an empty square
+#   check for winner
+#   player 2 picks an empty square
+#   check for winner
+
+# If there’s a winner
+#   show the winner
+# else
+#   it’s a tie
